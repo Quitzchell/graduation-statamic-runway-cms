e55e17081d245bec2a2768b03352b880d392c1f6
mitchellquitz <mitchell@allesonline.nl>
2024-12-23 17:11:47 +0100

    update tests

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -1,6 +1,6 @@
 <?php
 
-test('convertToEmbedUrl correctly converts YouTube URLs', function () {
+test('convertToEmbedYouTubeUrl correctly converts YouTube URLs', function () {
     $testUrls = [
         ["https://www.youtube.com/watch?v=OAZWXUkrjPc", "https://www.youtube.com/embed/OAZWXUkrjPc"],
         ["https://youtu.be/OAZWXUkrjPc", "https://www.youtube.com/embed/OAZWXUkrjPc"],
@@ -8,10 +8,21 @@ test('convertToEmbedUrl correctly converts YouTube URLs', function () {
         ["https://m.youtube.com/watch?v=OAZWXUkrjPc", "https://www.youtube.com/embed/OAZWXUkrjPc"],
         ["https://www.youtube.com/watch?v=OAZWXUkrjPc&feature=related", "https://www.youtube.com/embed/OAZWXUkrjPc"],
         ["https://youtube.com/watch?v=OAZWXUkrjPc", "https://www.youtube.com/embed/OAZWXUkrjPc"],
-        ["invalid_url", "Invalid YouTube URL"],
     ];
 
     foreach ($testUrls as [$input, $expected]) {
         expect(convertToEmbedYouTubeUrl($input))->toBe($expected);
     }
+});
+
+test('convertToEmbedYouTubeUrl throws when incorrect Youtube URL is given', function () {
+    $testUrls = [
+        "Invalid YouTube URL",
+        "https://www.example.com/not_a_youtube_video"
+];
+        foreach ($testUrls as $url) {
+            // Assert: Expect the exception to be thrown
+            expect(fn() => convertToEmbedYouTubeUrl($url))
+                ->toThrow(InvalidArgumentException::class, 'Invalid YouTube URL');
+        }
 });
