8682bb16fd9a20f0d26c460999e838398513c180
mitchellquitz <mitchell@allesonline.nl>
2024-12-28 23:22:37 +0100

    create BlogPost model

---
+++ app/Models/BlogPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use StatamicRadPack\Runway\Traits\HasRunwayResource;
+
+class BlogPost extends Model
+{
+    use HasRunwayResource;
+}
