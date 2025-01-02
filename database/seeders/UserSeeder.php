23f4064aca843a947f63d1ab211afb2701bda93f
mitchellquitz <mitchell@allesonline.nl>
2025-01-02 10:56:50 +0100

    update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,12 +10,24 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Mitchell Quitz',
-            'email' => 'mitchell@allesonline.nl',
-            'password' => Hash::make(config('services.admin')),
-            'super' => 1,
-            'email_verified_at' => now(),
-        ]);
+        $users = [
+            [
+                'name' => 'Mitchell Quitz',
+                'email' => 'mitchell@allesonline.nl',
+                'password' => Hash::make(config('services.admin')),
+                'super' => 1,
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'Test account',
+                'email' => 'test@allesonline.nl',
+                'password' => Hash::make(config('services.admin')),
+                'super' => 1,
+                'email_verified_at' => now(),
+            ]
+        ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
